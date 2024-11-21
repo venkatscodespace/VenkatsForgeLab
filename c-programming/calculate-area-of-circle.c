1b53f4e4a69e0d587ccf1f8112b2a464c57edf4e
Venkatram <venkatram.csc1@gmail.com>
2024-11-21 20:32:11 +0530

    Delete c-programming directory

--- c-programming/calculate-area-of-circle.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    float pi=3.14;
-    float radius;
-    printf("Input Radius:");
-    scanf("%f",&radius);
-    float result=pi*radius*radius;
-    printf("%f is the area of the circle with radius %f cm.",result,radius);
-}
