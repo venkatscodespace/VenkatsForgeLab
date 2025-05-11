d79ef3941acb0ff32a317f2cb163d14754d93238
Venkatram S <venkatram.csc1@gmail.com>
2025-05-11 23:46:28 +0530

    Create factorial.java

---
+++ java-practice/factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int c=1;
+	    int n=6;
+	    for (int i=1; i<n+1; i++) {
+	        c*=i;
+	        System.out.println(""+c);
+	    }
+	}
+}
