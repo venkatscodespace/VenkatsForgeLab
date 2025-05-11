8090a56cb6b81961e24402be4375c453d32a0964
Venkatram S <venkatram.csc1@gmail.com>
2025-05-11 23:40:03 +0530

    Create print1to10.java

---
+++ java-practice/print1to10.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    for (int i=1; i<11; i++) {
+	        System.out.println(i);
+	    }
+	}
+}
