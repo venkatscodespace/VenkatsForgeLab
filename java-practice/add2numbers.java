4dc53c95e1c809a77635ad231177f8af4e4c4650
Venkatram S <venkatram.csc1@gmail.com>
2025-05-11 23:30:40 +0530

    Create add2numbers.java

---
+++ java-practice/add2numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num1 = 10;
+		int num2 = 20;
+		int Result=num1+num2;
+		System.out.println("Result of 10 + 20 is: "+(Result));
+		
+
+	}
+}
