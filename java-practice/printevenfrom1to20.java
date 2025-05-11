656cd664592252e32afd3d8ffb425a5988bfd4e7
Venkatram S <venkatram.csc1@gmail.com>
2025-05-11 23:41:58 +0530

    Create printevenfrom1to20.java

---
+++ java-practice/printevenfrom1to20.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    for (int i=1; i<21; i++) {
+	        if (i%2==0) {
+	            System.out.println(i);
+	        }
+	    }
+	}
+}
